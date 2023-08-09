77f0aaa7b53cd822f2b5d1bfc4f1643d19059dd7
fred <fjluartes@gmail.com>
2023-08-09 18:55:35 +0800

    edits server/index

--- server/index.js
+++ server/index.js
@@ -1,32 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const logger = require('morgan');
-require('dotenv').config();
+const logger = require("morgan");
+require("dotenv").config();
 const port = process.env.SERVER_PORT;
-const connString = process.env.DB_STRING;
-const cors = require('cors');
+const connString = process.env.MONGO_URL_TEST;
+const cors = require("cors");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-mongoose.connect(connString, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(connString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', () => {
-  console.log('Connected to cloud database: MongoDB Atlas');
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Connected to cloud database: MongoDB Atlas");
 });
 
-const userRoutes = require('./routes/user');
-const postRoutes = require('./routes/post');
+const userRoutes = require("./routes/user");
+const postRoutes = require("./routes/post");
 
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Express');
+app.get("/", (req, res) => {
+  res.send("Welcome to Express");
 });
 
 app.listen(port || 4000, (err) => {
